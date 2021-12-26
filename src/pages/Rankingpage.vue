b153b4550cfe264d3f3d171bf40453db6503fd05
Jason <r0781387@student.thomasmore.be>
2021-12-26 13:23:11 +0100

    Update Rankingpage.vue

--- src/pages/Rankingpage.vue
+++ src/pages/Rankingpage.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div>
     <button @click="goToPage('home')">Go to home</button>
     <h1>Ranking</h1>
@@ -10,8 +10,30 @@
       </div>
     </div>
   </div>
-</template>
+</template>-->
+<template>
+  <div>
+    <button @click="goToPage('home')">Go to home</button>
+    <h1>Ranking</h1>
 
+    <table>
+    <tr>
+    <td>Ranking</td>
+    <td>Artist</td>
+    <td>Title</td>   
+    <td>Points</td>
+    </tr>
+    <tr v-for="(song,i) in songs" :key="i">
+    <td>{{i+1}}</td>
+    <td>{{song.artist.name}}</td>
+    <td>{{song.title}}</td>  
+    <td>{{song.allPoints}}</td>
+    </tr>
+    </table>
+
+  </div>
+  
+</template>
 <script>
     export default {
         name: "Rankingpage",
@@ -21,18 +43,16 @@
             }
         }
     }
-    
 </script>
 <script>
 export default {
   name: "Rankingpage",
-
   data() {
     return {
       songs: [],
-      votes: [],
     };
   },
+  
   computed: {
     total: function(){
       return this.items.reduce(function(prev, item){
@@ -41,49 +61,29 @@ export default {
     },
   },
 
-
   mounted() {
     this.fetchSongs();
-    this.fetchArtists();
-
-    this.fetchVO();
   },
-
   methods: {
     goToPage(page) {
       this.$emit("change-page", page);
     },
-
-    // data methods
+    //data methods
     fetchSongs() {
-      // Get all songs
+      //get all songs
       const url = "http://webservies.be/eurosong/Songs";
-
       fetch(url)
         .then((response) => {
           return response.json();
         })
-        .then((response) => (this.songs = response))
-        .then((votes) => {
-          this.fetchArtists(votes);
+        .then((songs) => {
+          this.fetchVotes(songs);
+          this.fetchArtists(songs);
         });
     },
-
-    fetchVO() {
-      // Get all songs
-      const url = "http://webservies.be/eurosong/Votes";
-
-      fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((response) => (this.votes = response));
-    },
-
     fetchArtists(songs) {
-      // Get all artist
+      //get all artist
       const url = "http://webservies.be/eurosong/Artists";
-
       fetch(url)
         .then((response) => {
           // response is text, so convert to json
@@ -92,48 +92,55 @@ export default {
         .then((artists) => {
           // loop over array songs with forEach method
           songs.map((song) => {
-            // find the artist in an array
+             // find the artist in an array
             const artist = artists.find((artist) => artist.id == song.artist);
-
-            // replace the id by the artist object
-            song.artist = artist;
-
-            // return the new object
+            song.artist = artist.name;
+             // return the new object
             return song;
           });
-
           // change data of songs, so everything will get rerenderd;
           this.songs = songs;
         });
     },
-
-    /*fetchVotes(songs) {
-      // Get all artist
+    /*fetchVO() {
+      // Get all songs
       const url = "http://webservies.be/eurosong/Votes";
-
       fetch(url)
         .then((response) => {
-          // response is text, so convert to json
+          return response.json();
+        })
+        .then((response) => (this.votes = response));
+    },*/
+
+    fetchVotes(songs) {
+      // Get all artist
+      const url = "http://webservies.be/eurosong/Votes";
+      
+      fetch(url)
+        .then((response) => {
+           // response is text, so convert to json
           return response.json();
         })
         .then((votes) => {
           // loop over array songs with forEach method
+          let allvotes = 0;
           songs.map((song) => {
-            // find the artist in an array
-            const vote = votes.find((vote) => vote.songID == song.id);
-          
-
-            // replace the id by the artist object
-            song.id = vote;
-
+            song.allPoints = 0;
+            votes
+              .filter((vote) => vote.songID == song.id)
+              .forEach((element) => {
+                if (element.songID == song.id) {
+                  song.allPoints += element.points;
+                  allvotes += element.points;
+                }
+              });
             // return the new object
             return song;
           });
-
           // change data of songs, so everything will get rerenderd;
-          this.songs = songs;
+          this.songs = songs.sort((a, b) => b.allPoints - a.allPoints);
         });
-    },*/
+    },
   },
 };
 </script>
